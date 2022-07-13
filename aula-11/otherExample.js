0570f01d1c50af3f2fca4123d7d0ce6ea8935530
devSantos16 <devsantos1999@gmail.com>
2022-07-13 00:13:03 -0300

    Lesson 11, Hoisting, Var and Let Examples (variables)

---
+++ aula-11/otherExample.js
@@ -0,0 +1,9 @@
+var firstName = 'Emanuel';
+var lastName = 'dos Santos Paz';
+
+if(firstName === 'Emanuel'){
+    var firstName = 'Pedro';
+    let lastName = 'Silva';
+}
+
+console.log(firstName, lastName);
